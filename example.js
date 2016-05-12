624747cfbbfbc4a2ac2b0207ba520409223570cd
cloned2k16 <paolo@yaoo.net>
2016-05-12 13:17:32 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,20 @@
+var w4it    =   require("w4it")
+,   request =   require("request")
+,   _       = {}
+;
+
+_.inProgress = true;
+
+request.get('http://ipinfo.io/json/', function (err, response, body) {
+    _.inProgress = false;
+    
+    if (!err && response.statusCode == 200) {
+        console.log(body);
+    }
+    else console.err(err);
+
+});
+            
+w4it.done ('inProgress',_);
+
+console.log('Done.');
